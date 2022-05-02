b9dd0cf8af71292a6efd562a0204b4a737b4b7a9
thespite <the.spite@gmail.com>
2022-05-02 20:04:57 +0100

    added updated slit scan

---
+++ modules/gum.js
@@ -0,0 +1,104 @@
+class Gum {
+  constructor(deviceNameElement, nextDeviceButton) {
+    this.deviceNameLabel = deviceNameElement;
+    this.nextDeviceButton = nextDeviceButton;
+    this.currentVideoInput = 0;
+    this.devices = {
+      audioinput: [],
+      audiooutput: [],
+      videoinput: [],
+    };
+    this.nextDeviceButton.addEventListener("click", (e) => {
+      this.currentVideoInput =
+        (this.currentVideoInput + 1) % this.devices.videoinput.length;
+      this.invalidateVideoSource();
+      this.getMedia(this.devices.videoinput[this.currentVideoInput]);
+    });
+
+    this.video = document.createElement("video");
+    this.video.autoplay = true;
+    this.video.playsinline = true;
+    this.video.addEventListener("loadeddata", () => {
+      this.resolveLoadedData();
+    });
+  }
+
+  async init() {
+    await this.enumerateDevices();
+    if (this.devices.videoinput.length === 1) {
+      this.nextDeviceButton.style.display = "none";
+      this.currentVideoInput = 0;
+      this.invalidateVideoSource();
+      this.getMedia(this.devices.videoinput[this.currentVideoInput]);
+    } else {
+      this.nextDeviceButton.style.display = "initial";
+      this.currentVideoInput = 0;
+      this.invalidateVideoSource();
+      this.getMedia(this.devices.videoinput[this.currentVideoInput]);
+    }
+  }
+
+  async ready() {
+    await this.videoLoadedData;
+  }
+
+  async enumerateDevices() {
+    if (!navigator.mediaDevices) {
+      this.deviceNameLabel.textContent = `Can't enumerate devices. Make sure the page is HTTPS, and the browser support getUserMedia.`;
+      return;
+    }
+    const devices = await navigator.mediaDevices.enumerateDevices();
+
+    for (const device of devices) {
+      let name;
+      switch (device.kind) {
+        case "audioinput":
+          name = device.label || "Microphone";
+          break;
+        case "audiooutput":
+          name = device.label || "Speakers";
+          break;
+        case "videoinput":
+          name = device.label || "Camera";
+          break;
+      }
+      this.devices[device.kind].push(device);
+    }
+  }
+
+  invalidateVideoSource() {
+    this.videoLoadedData = new Promise((resolve, reject) => {
+      this.resolveLoadedData = resolve;
+      this.rejectLoadedData = reject;
+    });
+  }
+
+  async getMedia(device) {
+    const constraints = {
+      video: { deviceId: device.deviceId, width: 500, height: 500 },
+    };
+    this.deviceNameLabel.textContent = "Connecting...";
+    let stream = null;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.deviceNameLabel.textContent = device.label
+        ? device.label
+        : "Unlabeled camera";
+      this.createVideoElement();
+      this.video.srcObject = stream;
+    } catch (err) {
+      this.deviceNameLabel.textContent = `${err.name} ${err.message}`;
+    }
+  }
+
+  createVideoElement() {
+    if (this.video && this.video.srcObject) {
+      this.video.srcObject.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+  }
+}
+
+export { Gum };
---
+++ slit-scan/main.js
@@ -0,0 +1,158 @@
+import { Gum } from "../modules/gum.js";
+
+const gum = new Gum(
+  document.querySelector("#device"),
+  document.querySelector("#nextDeviceBtn")
+);
+
+const isVertical = true;
+const size = 2;
+const lagBuffer = [];
+const canvas = document.getElementById("photo");
+const context = canvas.getContext("2d");
+let scanline = 0;
+let lagFrames;
+
+canvas.width = 0;
+document.body.appendChild(canvas);
+
+function gotStream() {
+  document.getElementById("message").hidden = true;
+
+  const video = gum.video;
+
+  console.log(video.videoWidth, video.videoHeight);
+  lagFrames = (isVertical ? video.videoHeight : video.videoWidth) / size;
+  for (var j = 0; j < lagFrames; j++) {
+    var c = document.createElement("canvas");
+    var ctx = c.getContext("2d");
+    lagBuffer.push({ canvas: c, ctx: ctx });
+  }
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  context.drawImage(
+    video,
+    0,
+    0,
+    video.videoWidth,
+    video.videoHeight,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  for (var j = 0; j < lagBuffer.length; j++) {
+    lagBuffer[j].canvas.width = video.videoWidth;
+    lagBuffer[j].canvas.height = video.videoHeight;
+
+    lagBuffer[j].ctx.drawImage(
+      video,
+      0,
+      0,
+      video.videoWidth,
+      video.videoHeight,
+      0,
+      0,
+      lagBuffer[j].canvas.width,
+      lagBuffer[j].canvas.height
+    );
+  }
+  scanline = 0;
+  onResize();
+
+  snapshotLagged();
+}
+
+function noStream() {
+  document.getElementById("message").innerHTML =
+    '<span class="error">No camera available.</span> Please allow the page access to your camera.';
+}
+
+function streamError() {
+  document.getElementById("message").innerHTML =
+    '<span class="error">Camera error.</span> Something went very wrong :(';
+}
+
+function mod(a, n) {
+  return ((a % n) + n) % n;
+}
+
+function snapshotLagged() {
+  const video = gum.video;
+  lagBuffer[scanline].ctx.drawImage(
+    video,
+    0,
+    0,
+    video.videoWidth,
+    video.videoHeight,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  if (isVertical) {
+    for (let y = 0; y < video.videoHeight / size; y++) {
+      const p = mod(scanline - y, lagFrames);
+      const i = lagBuffer[p].canvas;
+      context.drawImage(
+        i,
+        0,
+        y * size,
+        video.videoWidth,
+        size,
+        0,
+        y * size,
+        canvas.width,
+        size
+      );
+    }
+  } else {
+    for (let x = 0; x < video.videoWidth / size; x++) {
+      const p = mod(scanline - x, lagFrames);
+      const i = lagBuffer[p].canvas;
+      context.drawImage(
+        i,
+        x,
+        0,
+        size,
+        video.videoHeight,
+        x,
+        0,
+        size,
+        canvas.height
+      );
+    }
+  }
+  scanline = mod(scanline + 1, lagFrames);
+
+  requestAnimationFrame(snapshotLagged);
+}
+
+async function init() {
+  await gum.init();
+  await gum.ready();
+  gotStream();
+}
+init();
+
+window.addEventListener("resize", onResize);
+
+function onResize() {
+  const video = gum.video;
+  const ar = window.innerHeight / window.innerWidth;
+  const cr = video.videoHeight / video.videoWidth;
+
+  if (cr <= ar) {
+    canvas.style.width = window.innerWidth + "px";
+    canvas.style.height = window.innerWidth * cr + "px";
+  } else {
+    canvas.style.height = window.innerHeight + "px";
+    canvas.style.width = window.innerHeight / cr + "px";
+  }
+
+  canvas.style.marginLeft = -0.5 * canvas.clientWidth + "px";
+  canvas.style.marginTop = -0.5 * canvas.clientHeight + "px";
+}
