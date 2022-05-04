4119a544c62a4a38086f49d2b6da3f9cad06b0ec
thespite <the.spite@gmail.com>
2022-05-04 22:53:48 +0100

    Update main.js

--- glitch/main.js
+++ glitch/main.js
@@ -4,7 +4,7 @@ import {
   OrthographicCamera,
   PlaneBufferGeometry,
   RawShaderMaterial,
-  CanvasTexture,
+  VideoTexture,
   LinearFilter,
   NearestFilter,
   RGBAFormat,
@@ -22,25 +22,9 @@ const gum = new Gum(
   document.querySelector("#nextDeviceBtn")
 );
 
-Number.prototype.sign = function () {
-  if (this < 0) {
-    return -1;
-  }
-  if (this > 0) {
-    return 1;
-  }
-  return 0;
-};
+const scale = 2;
 
-var scale = 2;
-var fStep = scale;
-var wStep = fStep * fStep + 1;
-
-var camera,
-  renderer,
-  container = document.getElementById("container");
-
-var scene, mesh;
+const container = document.getElementById("container");
 
 const canvas = document.getElementById("photo");
 const context = canvas.getContext("2d");
@@ -49,33 +33,65 @@ const oCtx = oCanvas.getContext("2d");
 const tCanvas = document.createElement("canvas");
 const tCtx = tCanvas.getContext("2d");
 
-var offsets, momentum, offsetTexture;
-var oW, oH;
+let offsets;
+let momentum;
+let offsetTexture;
+let oW;
+let oH;
 
-var videoTexture = new CanvasTexture(oCanvas);
+const videoTexture = new VideoTexture(gum.video);
 videoTexture.minFilter = NearestFilter;
 videoTexture.magFilter = LinearFilter;
 
 canvas.width = 0;
-var flow = new FlowCalculator(fStep);
-var startTime;
+const flow = new FlowCalculator(scale / 4);
+let startTime;
+
+const renderer = new WebGLRenderer();
+container.appendChild(renderer.domElement);
+
+const scene = new Scene();
+
+const camera = new OrthographicCamera(
+  1 / -2,
+  1 / 2,
+  1 / 2,
+  1 / -2,
+  0.00001,
+  1000
+);
+
+scene.add(camera);
+
+const mesh = new Mesh(
+  new PlaneBufferGeometry(1, 1),
+  new RawShaderMaterial({
+    uniforms: {
+      map: { value: videoTexture },
+      offsetMap: { value: offsetTexture },
+    },
+    vertexShader,
+    fragmentShader,
+    glslVersion: GLSL3,
+  })
+);
+
+scene.add(mesh);
 
 function start() {
-  oCanvas.style.display = "block";
   startTime = Date.now();
   extractDifferences();
 }
 
-function update(d, f, data) {
-  for (var j = 0; j < d.zones.length; j++) {
-    var ptr = d.zones[j].y / fStep;
-    var z = d.zones[j];
-    var s = wStep;
-    var x = Math.floor(z.x / s);
-    var y = Math.floor(z.y / s);
-    var ptr = y * oW * 4 + x * 4;
+function update(d) {
+  for (let j = 0; j < d.zones.length; j++) {
+    const z = d.zones[j];
+    const s = scale;
+    const x = Math.round(z.x / s);
+    const y = Math.round(z.y / s);
+    const ptr = y * oW * 4 + x * 4;
 
-    var m = 0.95;
+    const m = 0.95;
     momentum[ptr] += z.u;
     momentum[ptr] *= m;
     momentum[ptr + 1] += z.v;
@@ -110,9 +126,12 @@ function extractDifferences() {
   ) {
     canvas.width = oCanvas.width = tCanvas.width = video.videoWidth / scale;
     canvas.height = oCanvas.height = tCanvas.height = video.videoHeight / scale;
-    initOffsets(canvas.width / wStep, canvas.height / wStep);
+    initOffsets(canvas.width / scale, canvas.height / scale);
     onResize();
   }
+
+  let iData;
+  let oData;
 
   try {
     context.drawImage(
@@ -127,29 +146,25 @@ function extractDifferences() {
       canvas.height
     );
     if (canvas.width > 0 && canvas.height > 0) {
-      //stackBlurCanvasRGB( canvas, 0, 0, canvas.width, canvas.height, 10 );
       tCtx.drawImage(canvas, 0, 0);
     }
 
-    var iData = context.getImageData(0, 0, canvas.width, canvas.height);
-    var oData = oCtx.getImageData(0, 0, oCanvas.width, oCanvas.height);
+    iData = context.getImageData(0, 0, canvas.width, canvas.height);
+    oData = oCtx.getImageData(0, 0, oCanvas.width, oCanvas.height);
   } catch (e) {
     requestAnimationFrame(extractDifferences);
     return;
   }
 
-  var p = iData.data.length / 4;
-  var buf = new ArrayBuffer(iData.data.length);
-  var oBuf = new ArrayBuffer(oData.data.length);
-  var buf8 = new Uint8ClampedArray(iData.data);
-  var oBuf8 = new Uint8ClampedArray(oData.data);
+  const buf8 = new Uint8ClampedArray(iData.data);
+  const oBuf8 = new Uint8ClampedArray(oData.data);
 
-  var d = flow.calculate(buf8, oBuf8, canvas.width, canvas.height);
+  const d = flow.calculate(buf8, oBuf8, canvas.width, canvas.height);
 
-  var currentTime = Date.now();
-  var eTime = currentTime - startTime;
+  const currentTime = Date.now();
+  const eTime = currentTime - startTime;
   startTime = currentTime;
-  var f = eTime / 16;
+  const f = eTime / 16;
 
   update(d, f);
 
@@ -157,45 +172,6 @@ function extractDifferences() {
 
   requestAnimationFrame(extractDifferences);
 }
-
-function initWebGL() {
-  renderer = new WebGLRenderer();
-  container.appendChild(renderer.domElement);
-
-  scene = new Scene();
-
-  camera = new OrthographicCamera(1 / -2, 1 / 2, 1 / 2, 1 / -2, 0.00001, 1000);
-
-  scene.add(camera);
-
-  mesh = new Mesh(
-    new PlaneBufferGeometry(1, 1),
-    new RawShaderMaterial({
-      uniforms: {
-        map: { value: videoTexture },
-        offsetMap: { value: offsetTexture },
-      },
-      vertexShader,
-      fragmentShader,
-      glslVersion: GLSL3,
-    })
-  );
-
-  scene.add(mesh);
-
-  onResize();
-}
-
-async function init() {
-  await gum.init();
-  await gum.ready();
-
-  initWebGL();
-  start();
-}
-init();
-
-window.addEventListener("resize", onResize);
 
 function onResize() {
   const video = gum.video;
@@ -221,3 +197,13 @@ function onResize() {
 
   camera.updateProjectionMatrix();
 }
+
+async function init() {
+  await gum.init();
+  await gum.ready();
+  window.addEventListener("resize", onResize);
+  onResize();
+  start();
+}
+
+init();
