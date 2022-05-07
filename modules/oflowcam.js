d690efc75f3ecd8f5424c065a69942fe40f91824
thespite <the.spite@gmail.com>
2022-05-08 00:42:36 +0100

    Update oflowcam.js

--- modules/oflowcam.js
+++ modules/oflowcam.js
@@ -4,16 +4,16 @@ import { Gum } from "./gum.js";
 let flow;
 let scale = 2;
 
-const canvas = document.getElementById("photo");
-const context = canvas.getContext("2d");
-canvas.width = 0;
+let width = 0;
+let height = 0;
 
-const oCanvas = document.createElement("canvas");
-const oCtx = oCanvas.getContext("2d");
-const tCanvas = document.createElement("canvas");
-const tCtx = tCanvas.getContext("2d");
+const newImage = document.createElement("canvas");
+const newImageContext = newImage.getContext("2d");
+const oldImage = document.createElement("canvas");
+const oldImageContext = oldImage.getContext("2d");
 
-let startTime;
+document.body.append(newImage);
+document.body.append(oldImage);
 
 const gum = new Gum(
   document.querySelector("#device"),
@@ -25,19 +25,21 @@ let onSourceResize = () => {};
 function extractDifferences() {
   const video = gum.video;
   if (
-    canvas.width != video.videoWidth / scale ||
-    canvas.height != video.videoHeight / scale
+    width != video.videoWidth / scale ||
+    height != video.videoHeight / scale
   ) {
-    canvas.width = oCanvas.width = tCanvas.width = video.videoWidth / scale;
-    canvas.height = oCanvas.height = tCanvas.height = video.videoHeight / scale;
-    onSourceResize(canvas.width / scale, canvas.height / scale);
+    width = video.videoWidth / scale;
+    height = video.videoHeight / scale;
+    newImage.width = oldImage.width = width;
+    newImage.height = oldImage.height = height;
+    onSourceResize(width / scale, height / scale);
   }
 
-  let iData;
-  let oData;
+  let oldImageData;
+  let newImageData;
 
   try {
-    context.drawImage(
+    newImageContext.drawImage(
       video,
       0,
       0,
@@ -45,32 +47,21 @@ function extractDifferences() {
       video.videoHeight,
       0,
       0,
-      canvas.width,
-      canvas.height
+      width,
+      height
     );
-    if (canvas.width > 0 && canvas.height > 0) {
-      tCtx.drawImage(canvas, 0, 0);
-    }
 
-    iData = context.getImageData(0, 0, canvas.width, canvas.height);
-    oData = oCtx.getImageData(0, 0, oCanvas.width, oCanvas.height);
+    oldImageData = oldImageContext.getImageData(0, 0, width, height);
+    newImageData = newImageContext.getImageData(0, 0, width, height);
   } catch (e) {
     return { d: null, f: null };
   }
 
-  const buf8 = new Uint8ClampedArray(iData.data);
-  const oBuf8 = new Uint8ClampedArray(oData.data);
+  const d = flow.calculate(oldImageData.data, newImageData.data, width, height);
 
-  const d = flow.calculate(buf8, oBuf8, canvas.width, canvas.height);
+  oldImageContext.drawImage(newImage, 0, 0);
 
-  const currentTime = Date.now();
-  const eTime = currentTime - startTime;
-  startTime = currentTime;
-  const f = eTime / 16;
-
-  oCtx.drawImage(tCanvas, 0, 0);
-
-  return { d, f };
+  return { d };
 }
 
 async function init(s, onSourceResizeCb = onSourceResize) {
@@ -79,8 +70,7 @@ async function init(s, onSourceResizeCb = onSourceResize) {
   onSourceResize = onSourceResizeCb;
   await gum.init();
   await gum.ready();
-  startTime = Date.now();
-  return gum.video;
+  return { video: gum.video, canvas: newImage };
 }
 
 export { init, flow, extractDifferences, scale };
